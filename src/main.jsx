c006c44f49d58cfdfea138e2efac1b04fc1edd8e
Fernanda Valenzuela <fernandaisabellavalenzuelac@gmail.com>
2025-05-27 10:07:04 -0400

    Reestructuracion repo  e instalacion bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './components/Footer/Footer.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Footer/>
   </StrictMode>,
 )
