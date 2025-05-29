e610d8e09a4fd04f0b546b20582e274071bcc0d5
Margarita Pvz <margaritaspvz@gmail.com>
2025-05-29 17:28:10 -0400

    Estructuración Categorias

---
+++ src/Pages/Categories.jsx
@@ -0,0 +1,49 @@
+<div className="container  py-3">
+        <h1 className="section-title display-5">Nuestras categorías</h1>
+        <div className="row row-cols-2 mt-2 row-cols-lg-3 text-center gx-lg-4">
+            <div className="col">
+                <div className="card mycard ingles">
+                    <img src="./assets/img/english.png" className="card-img-top mx-auto" alt="...">
+                    <div className="card-body">
+                        <h5 className="card-title">Inglés</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- categoría 2 -->
+            <div className="col">
+                <div className="card mycard historia">
+                    <img src="./assets/img/history.png" className="card-img-top mx-auto" alt="...">
+                    <div className="card-body">
+                        <h5 className="card-title">Historia</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- categoría 3 -->
+            <div className="col">
+                <div className="card mycard matematicas">
+                    <img src="./assets/img/maths.png" className="card-img-top mx-auto" alt="...">
+                    <div className="card-body">
+                        <h5 className="card-title">Matemáticas</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- categoría 4 -->
+            <div className="col">
+                <div className="card mycard ciencias">
+                    <img src="./assets/img/science.png" className="card-img-top mx-auto" alt="...">
+                    <div className="card-body">
+                        <h5 className="card-title">Ciencias</h5>
+                    </div>
+                </div>
+            </div>
+            <!-- categoría 5 -->
+            <div className="col">
+                <div className="card mycard lenguaje">
+                    <img src="./assets/img/spanish.png" className="card-img-top mx-auto" alt="...">
+                    <div className="card-body">
+                        <h5 className="card-title">Lenguaje</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
