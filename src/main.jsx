f12945151391c173d1ad350a229b6b45da74666e
Fernanda Valenzuela <fernandaisabellavalenzuelac@gmail.com>
2025-05-29 20:59:28 -0400

    juego Wordle version inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Footer from './components/Footer/Footer.jsx'
-import Index from './Pages/Index.jsx'
-import NavBar from './components/NavBar/NavBar.jsx'
-import TimeLGame from './components/TimeLGame/TimeLGame.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {RouterProvider} from 'react-router-dom'
 import { router } from './routes'
