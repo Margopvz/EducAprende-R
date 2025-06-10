fcbe5b386b1c3d7c8ee029cb6559e3e826349b7b
Diego Palma <diego.palma@usach.cl>
2025-06-10 17:46:35 -0400

    juego de matematicas completo

--- src/components/CategoryRouter.jsx
+++ src/components/CategoryRouter.jsx
@@ -2,10 +2,14 @@ import { useParams } from "react-router-dom";
 import TimeLGame from "./TimeLGame/TimeLGame";
 import WordleGame from "./WordleGame/WordleGame"
 import SopaDeLetras from "./SopaDeLetras/sopaDeLetras";
+import MathGame from '../features/math-game/MathGame';
+
 const CategoryRouter = () => {
   const { asignatura } = useParams();
 
   switch (asignatura) {
+    case "matematicas":
+      return <MathGame />;
     case "historia":
       return <TimeLGame />;
     case "lenguaje":
